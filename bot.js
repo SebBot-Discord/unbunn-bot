d1d7b77527957549cc99adde38b886d89ac7db3d
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-12-15 11:23:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -521,7 +521,7 @@ try {
     };
     if (message.content.substr(0,12) == "bunn!lookup "){
 	    var f = "Failure to serve request";
-	    request(process.env.API_KEY + skap, function(error, response, body){
+	    request(process.env.API_KEY + message.content.substr(12), function(error, response, body){
 		    var data = JSON.parse(body)
 		    var _fields = [
 		      {
